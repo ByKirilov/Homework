fbd9f63a339bdaf5a8b13fbb22aa9a12f192318a
ByKirilov <74AK333@gmail.com>
2018-12-17 04:30:51 +0500

    Some revisions

--- Assembler/test_task/sb.s
+++ Assembler/test_task/sb.s
@@ -1,50 +1,97 @@
 .globl _start
 .data
-tmp_number:		.skip 100
+.set 	N, 10
+.set	WRITE, 1
+empty:	.skip N
+tmp_number:		.skip N
+operands_count: 	.byte 0x0
 unary_minus:	.ascii "#"
+minus: 	.ascii "-"
 other: 	.ascii "w"
 bad:	.ascii "-\n"
 my_str:	.ascii "#123"
-zero:	.byte 0x21
+zero:	.ascii "!"
+n_l: 	.ascii "\n"
+char_tab:	.ascii "0123456789"
 .text
 _start:
-	lea	my_str, %rsi
-	lea	tmp_number, %rdi
-	mov 	$4, %rcx
-	rep movsb
-	call 	_parse_to_int
-	nop
-	jmp 	_exit
+	lea 	my_str, %rsi
+ 	lea 	tmp_number, %rdi
+ 	mov 	$4, %rcx
+ 	rep movsb
+ 	call  	_parse_to_int
+ 	jmp 	_numprint
+ 	jmp  	_exit
 
 _parse_to_int:
-	lea 	tmp_number, %rsi	# Берём адрес начала строки с числом
-	lea 	unary_minus, %rdi	# Адрес символа, отвечающего за унарный минус
-	cmpsb				# Проверяем, не является ли первый символ унарным минусом
-	jnz	_ii1
-	mov 	$1, %di			# Если является, ставим флаг
-	inc	%rsi			# И пропускаем этот минус
+ 	lea  	tmp_number, %rsi # Берём адрес начала строки с числом
+ 	lea  	unary_minus, %rdi # Адрес символа, отвечающего за унарный минус
+ 	cmpsb    # Проверяем, не является ли первый символ унарным минусом
+ 	jnz 	_ii1
+ 	mov  	$1, %di   # Если является, ставим флаг
+ 	inc 	%rsi   # И пропускаем этот минус
 _ii1:
-	dec 	%rsi
-	xorq 	%rax, %rax
-	mov 	$10, %rbx		# Основание СС
+ 	dec  	%rsi
+ 	xorq  	%rax, %rax
+ 	mov  	$10, %rbx  # Основание СС
 _ii2:
-	mov 	(%rsi), %cl 		# Берём символ из буфера
-	cmp 	$0, %cl 		# проверяем, не конец ли это строки
-	jz 	_end_number 		
+ 	mov  	(%rsi), %cl   # Берём символ из буфера
+ 	cmp  	$0, %cl   # проверяем, не конец ли это строки
+ 	jz  	_end_number   
 
-	sub 	$'0', %cl 		# Превращаем символ в число
-	mul 	%rbx 			# Умножаем то, что уже есть на основание СС
-	add 	%rcx, %rax 		# Добавляем к тому, что уже есть, то что получили на этом шаге
-	inc 	%rsi			# Указатель на следующий символ
-	jmp 	_ii2 			# Повторяем
+ 	sub  	$'0', %cl   # Превращаем символ в число
+ 	mul  	%rbx    # Умножаем то, что уже есть на основание СС
+ 	add  	%rcx, %rax   # Добавляем к тому, что уже есть, то что получили на этом шаге
+ 	inc  	%rsi   # Указатель на следующий символ
+ 	jmp  	_ii2    # Повторяем
 _end_number:
-	cmp 	$1, %di 		# Все символы из строки обработаны. Если установлен флаг, то
-	jnz 	_ii3
-	neg 	%rax			# делаем число отрицательным
+ 	cmp  	$1, %di   # Все символы из строки обработаны. Если установлен флаг, то
+ 	jnz  	_ii3
+ 	neg  	%rax   # делаем число отрицательным
 _ii3:
-	ret
+ 	ret
 # По итогу число лешит в RAX
 
 _exit:
-	mov 	$60, %rax
+ 	mov  $60, %rax
+ 	syscall
+
+_numprint:
+	cmp 	$0, %rax 	# Проверяем знак числа
+	jge 	_0b
+	pushq 	%rax 		# Если отрицательное, сохраняем rax
+	mov 	$WRITE, %rax
+	mov 	$1, %rdi
+	mov 	$minus, %rsi
+	mov 	$1, %rdx
+	syscall			# Печатаем минус
+	popq 	%rax 		# Возвращаем rax на место
+	neg 	%rax 		# Переводим число в положительное
+_0b:
+	xor	%rcx, %rcx
+	movq	$10, %rbx
+_1b:
+	xorq	%rdx, %rdx
+	div	%rbx
+	pushq	%rdx
+	inc	%rcx
+	testq	%rax, %rax
+	jnz	_1b
+_2b:
+	popq	%rax
+	lea	char_tab, %rsi
+	add 	%rax, %rsi
+	push 	%rcx
+	mov 	$WRITE, %rax
+	mov 	$1, %rdi
+	mov 	$1, %rdx
 	syscall
+	pop 	%rcx
+	dec	%rcx
+	jnz	_2b
+	lea 	n_l, %rsi
+	mov 	$WRITE, %rax
+	mov 	$1, %rdi
+	mov 	$1, %rdx
+	syscall
+	jmp	_exit
--- Assembler/test_task/task.s
+++ Assembler/test_task/task.s
@@ -6,6 +6,7 @@
 operands_count:		.word 0
 operation_symbol:	.ascii "?"
 tmp_number:		.skip N
+empty: 			.skip N
 current_number:		.quad 0
 current_numeral:	.byte 0
 numeral_char_tab:	.ascii "0123456789"
@@ -14,12 +15,32 @@ op_add:	.ascii "+"
 op_sub:	.ascii "-"
 op_mov:	.ascii "*"
 op_div:	.ascii "/"
+line_break:	.ascii "\n"
 err_messg:	.ascii "Something wrong\n"
 lerr_messg = . - err_messg
 .text
 _start:
-	
+	xorq 	%r10, %r10
 
+
+_push_num_to_stack:
+	lea 	tmp_number, %rsi
+	cmp 	$0, (%rsi)
+	jz 	_end_push
+	call 	_parse_to_int
+	pushq 	%rax
+	inc 	%r10
+	call	_clear_string
+_end_push:
+	ret
+# ---------------------------------------------------
+_clear_string:
+	lea	empty, %rsi
+	lea 	tmp_number, %rdi
+	mov 	$N, %rcx
+	rep movsb
+	ret
+# ---------------------------------------------------
 _parse_to_int:
 	lea 	tmp_number, %rsi
 	jmp	_ii1
@@ -38,31 +59,46 @@ _ii2:
 	jmp 	_ii2
 _end_number:
 	ret
-
+# ---------------------------------------------------
 add_op:
+	cmp	$2, %r10
+	jl 	_error
 	popq	%rax
 	popq 	%rbx
 	add 	%rbx, %rax
 	pushq	%rax
 	ret
 sub_op:
+	cmp	$2, %r10
+	jl 	_error
 	popq	%rax
 	popq 	%rbx
 	sub 	%rbx, %rax
 	pushq	%rax
 	ret
 mul_op:
+	cmp	$2, %r10
+	jl 	_error
 	popq	%rax
 	popq 	%rbx
 	mul 	%rbx
 	pushq	%rax
 	ret
 div_op:
+	cmp	$2, %r10
+	jl 	_error
 	popq	%rax
 	popq 	%rbx
 	div 	%rbx
 	pushq 	%rax
 	ret
+unary_minus:
+	cmp	$1, %r10
+	jl 	_error
+	popq 	%rax
+	neg 	%rax
+	pushq 	%rax
+# ---------------------------------------------------
 _error:
 	mov	$WRITE, %rax
 	mov	$1, %rdi
@@ -70,23 +106,36 @@ _error:
 	mov	$lerr_messg, %rdx
 	syscall
 	jmp	_exit
+# ---------------------------------------------------
 _exit:
 	mov	$EXIT, %rax
 	mov	$0, %rdi
 	syscall
+# ---------------------------------------------------
 _numprint:
+	cmp 	$0, %rax
+	jge 	_0b
+	pushq 	%rax
+	mov 	$WRITE, %rax
+	mov 	$1, %rdi
+	mov 	$op_sub, %rsi
+	mov 	$1, %rdx
+	syscall
+	popq 	%rax
+	neg 	%rax
+_0b:
 	xor	%rcx, %rcx
 	movq	$10, %rbx
-1:
+_1b:
 	xorq	%rdx, %rdx
 	div	%rbx
 	pushq	%rdx
 	inc	%rcx
 	testq	%rax, %rax
-	jnz	1b
-2:
+	jnz	_1b
+_2b:
 	popq	%rax
-	lea	char_tab, %rsi
+	lea	numeral_char_tab, %rsi
 	add 	%rax, %rsi
 	push 	%rcx
 	mov 	$WRITE, %rax
@@ -95,7 +144,7 @@ _numprint:
 	syscall
 	pop 	%rcx
 	dec	%rcx
-	jnz	2b
+	jnz	_2b
 	lea 	line_break, %rsi
 	mov 	$WRITE, %rax
 	mov 	$1, %rdi
