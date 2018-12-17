2d51ba73f4f2869c2a9bc008278a01a6041e2907
ByKirilov <74AK333@gmail.com>
2018-12-17 09:06:18 +0500

    Need for debug

--- Assembler/test_task/task.s
+++ Assembler/test_task/task.s
@@ -4,25 +4,106 @@
 .set	WRITE, 1
 .set	EXIT, 60
 operands_count:		.word 0
-operation_symbol:	.ascii "?"
+operation_symbol:	.skip 1
 tmp_number:		.skip N
-empty: 			.skip N
+current_numeral:	.skip 1
+big_empty: 		.skip N
+small_empty: 		.skip 1
 current_number:		.quad 0
-current_numeral:	.byte 0
 numeral_char_tab:	.ascii "0123456789"
-operration_char_tab:	.ascii "+-*/#"
+numerals_count = 	. - numeral_char_tab
+operation_char_tab:	.ascii "+-*/#"
+operations_count = 	. - operation_char_tab
 op_add:	.ascii "+"
 op_sub:	.ascii "-"
 op_mov:	.ascii "*"
 op_div:	.ascii "/"
+op_unary_minus: .ascii "#"
 line_break:	.ascii "\n"
 err_messg:	.ascii "Something wrong\n"
 lerr_messg = . - err_messg
+
 .text
 _start:
-	xorq 	%r10, %r10
+	xorq 	%r10, %r10		# r10 играет роль счётчика количества операндов на стеке
+	mov 	(%rsp), %r11
+	dec 	%r11 			# r11 показывает сколько аргументов нам передали
+	cmp 	$0, %r11
+	jz 	_error
 
+	lea 	tmp_number, %r12 	# r12 - указатель на конец строки tmp_number
+	mov	$2, %rdx
+	mov	(%rsp, %rdx, 8), %r14	# r14 - текущий аргумент
+	jmp 	_parse_argument
 
+_result:
+	cmp 	$1, %r10
+	jg 	_error
+	popq 	%rax
+	jmp 	_numprint
+# ---------------------------------------------------
+_parse_argument:
+	xor 	%rbx, %rbx
+	mov 	(%r14), %bl
+	cmp 	$0, %bl
+	jz 	_end_argument
+
+	call 	_grasp_operation_symbol
+	call 	_execute_operation
+
+	call 	_grasp_numeral
+
+	inc 	%r14
+	jmp 	_parse_argument
+_end_argument:
+	call 	_push_num_to_stack
+	dec 	%r11
+	jz 	_result
+	mov 	$1, %rdx
+	mov	(%rsp, %rdx, 8), %r14
+	jmp 	_parse_argument
+# ---------------------------------------------------
+ _grasp_operation_symbol:
+ 	mov 	$operations_count, %r15
+	mov 	$1, %rcx
+	mov	%r14, %rsi
+	lea	operation_char_tab, %rdi
+ _grasp_operation_loop:
+	cmpsb
+	je	_end_grasp_operation
+	dec	%rsi
+	dec 	%r15
+	jnz 	_grasp_operation_loop
+	ret
+_end_grasp_operation:
+	dec 	%rdi
+	mov 	%rdi, %rsi
+	lea 	operation_symbol, %rdi
+	mov 	$1, %rcx
+	rep movsb
+	ret
+# ---------------------------------------------------
+_grasp_numeral:
+	mov 	$numerals_count, %r15
+	mov 	$1, %rcx
+	mov 	%r14, %rsi
+	lea 	numeral_char_tab, %rdi
+_grasp_numeral_loop:
+	cmpsb
+	je	_end_grasp_numeral
+	dec	%rsi
+	dec 	%r15
+	jnz 	_grasp_numeral_loop
+	jmp 	_error
+_end_grasp_numeral:
+	dec 	%rdi
+	mov 	%rdi, %rsi
+	mov 	%r12, %rdi
+	mov 	$1, %rcx
+	rep movsb
+	inc 	%r12
+	ret
+# ---------------------------------------------------
 _push_num_to_stack:
 	lea 	tmp_number, %rsi
 	cmp 	$0, (%rsi)
@@ -30,14 +111,26 @@ _push_num_to_stack:
 	call 	_parse_to_int
 	pushq 	%rax
 	inc 	%r10
-	call	_clear_string
+	call	_clear_tmp_number
 _end_push:
 	ret
 # ---------------------------------------------------
-_clear_string:
-	lea	empty, %rsi
+_clear_tmp_number:
+	lea	big_empty, %rsi
 	lea 	tmp_number, %rdi
 	mov 	$N, %rcx
+	jmp 	_clear_string
+_clear_operation_symbol:
+	lea	small_empty, %rsi
+	lea 	operation_symbol, %rdi
+	mov 	$1, %rcx
+	jmp 	_clear_string
+_clear_current_numeral:
+	lea	small_empty, %rsi
+	lea 	current_numeral, %rdi
+	mov 	$1, %rcx
+	jmp 	_clear_string
+_clear_string:
 	rep movsb
 	ret
 # ---------------------------------------------------
@@ -58,6 +151,43 @@ _ii2:
 	inc 	%rsi
 	jmp 	_ii2
 _end_number:
+	lea 	tmp_number, %r12
+	ret
+# ---------------------------------------------------
+_execute_operation:
+	mov 	$operation_symbol, %rsi
+	mov 	(%rsi), %cl
+	inc 	%cl
+	cmp 	$1, %cl
+#	test 	%cl, %cl
+	je 	_end_execute_operation
+
+	call 	_push_num_to_stack
+
+	mov 	$operation_symbol, %rsi
+	mov 	$1, %rcx
+	lea	op_add, %rdi
+	cmpsb
+	je	add_op
+	dec	%rsi
+	lea	op_sub, %di
+	cmpsb
+	je	sub_op
+	dec	%rsi
+	lea	op_mov, %di
+	cmpsb
+	je	mul_op
+	dec	%rsi
+	lea	op_div, %di
+	cmpsb
+	je	div_op
+	dec	%rsi
+	lea	op_unary_minus, %di
+	cmpsb
+	je	unary_minus
+	dec	%rsi
+_end_execute_operation:
+	call _clear_operation_symbol
 	ret
 # ---------------------------------------------------
 add_op:
@@ -67,7 +197,8 @@ add_op:
 	popq 	%rbx
 	add 	%rbx, %rax
 	pushq	%rax
-	ret
+	dec 	%r10
+	call 	_end_execute_operation
 sub_op:
 	cmp	$2, %r10
 	jl 	_error
@@ -75,7 +206,8 @@ sub_op:
 	popq 	%rbx
 	sub 	%rbx, %rax
 	pushq	%rax
-	ret
+	dec 	%r10
+	call 	_end_execute_operation
 mul_op:
 	cmp	$2, %r10
 	jl 	_error
@@ -83,7 +215,8 @@ mul_op:
 	popq 	%rbx
 	mul 	%rbx
 	pushq	%rax
-	ret
+	dec 	%r10
+	call 	_end_execute_operation
 div_op:
 	cmp	$2, %r10
 	jl 	_error
@@ -91,13 +224,15 @@ div_op:
 	popq 	%rbx
 	div 	%rbx
 	pushq 	%rax
-	ret
+	dec 	%r10
+	call 	_end_execute_operation
 unary_minus:
 	cmp	$1, %r10
 	jl 	_error
 	popq 	%rax
 	neg 	%rax
 	pushq 	%rax
+	call 	_end_execute_operation
 # ---------------------------------------------------
 _error:
 	mov	$WRITE, %rax
